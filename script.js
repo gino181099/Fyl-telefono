fa3baa4de8b148026ec1ba980395fb8f63dcc670
Gino Siculo <ginofranco2010@live.com>
2021-08-23 09:11:03 -0300

    prueba2

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ var M2 = TweenMax.to('.character2',1,{repeat:0,x:-24995,ease:SteppedEase.config(
 var M4 = TweenMax.to('.character4',10,{repeat:-1,x:-64000,ease:SteppedEase.config(128), paused: true});
 var M5 = TweenMax.to('.character5',8,{repeat:0,x:-51000,ease:SteppedEase.config(102), paused: true});
 var M6 = TweenMax.to('.character6',7,{repeat:0,x:-60000,ease:SteppedEase.config(120), paused: true});
-// var M7 = TweenMax.to('.character7',1,{repeat:0,x:-9500,ease:SteppedEase.config(19), paused: true});
+var M7 = TweenMax.to('.character7',1,{repeat:0,x:-9500,ease:SteppedEase.config(19), paused: true});
 
 // Control functions ( you can add whatever you want ) : 
 
@@ -31,51 +31,88 @@ function infinity(){  M2.repeat(-1); };
 // };
 
 const tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: ".uno",
-      start: "center center",
-      pin: true,
-      scrub: true,
-      end: "bottom top",
-      onEnter: () => M2.play(),
-      onLeaveBack: () => M2.reverse()
-    }
-  }).to(".text-one", {opacity: 1}).to('.character2', {opacity: 1, duration: 2});
-  
+  scrollTrigger:{
+    trigger: ".animaciones",
+    start: "center center",
+    end: "500% bottom",
+    scrub: true,
+    pin: true,
+    // markers: true,
+    pinSpacing: false
+  }
+}).to('.character2',1,{repeat:0,x:-24995,ease:SteppedEase.config(49)})
+  .to("#dos", {opacity: 1}).to('.uno',{ opacity: 0})
+  .to('.character4',10,{repeat:0,x:-64000,ease:SteppedEase.config(128)})
+  .to(".tres", {opacity: 1}).to('#dos',{ opacity: 0})
+  .to('.character5',8,{repeat:0,x:-51000,ease:SteppedEase.config(102)})
+  .to(".cuatro", {opacity: 1}).to('.tres',{ opacity: 0})
+  .to('.character6',7,{repeat:0,x:-60000,ease:SteppedEase.config(120)})
+  .to(".cuatro", {opacity:0});
+
   const tl2 = gsap.timeline({
-    scrollTrigger: {
-      trigger: "#dos",
+    scrollTrigger:{
+      trigger: ".animaciones",
       start: "center center",
-      pin: true,
+      end: "500% bottom",
       scrub: true,
-      end: "bottom top",
-      onEnter: () => M4.play(),
-      onEnterBack: () => M4.restart(),
-      onLeave: () => M4.pause()
-    }
-  }).to(".text-two", {opacity: 1}).to(".text-two", {opacity: 0});
-  const tl3 = gsap.timeline({
-    scrollTrigger: {
-      trigger: ".tres",
-      start: "center center",
       pin: true,
-      scrub: true,
-      end: "bottom top",
-      onEnter: () => M5.restart(),
-      onEnterBack: () => M5.restart()
+      markers: true,
+      pinSpacing: false
     }
-  }).to(".text-three", {opacity: 1}).to('.character4', {opacity: 1, duration: 2}).to('.character4', {opacity: 0}).to(".text-three", {opacity: 0});
-  const tl4 = gsap.timeline({
-    scrollTrigger: {
-      trigger: ".cuatro",
-      start: "center center",
-      pin: true,
-      scrub: true,
-      end: "bottom top",
-      onEnter: () => M6.restart(),
-      onEnterBack: () => M6.restart()
-    }
-  }).to(".text-four", {opacity: 1}).to('.character5', {opacity: 1, duration: 2}).to('.character5', {opacity: 0}).to(".text-four", {opacity: 0});
+  }).to(".text-one",{opacity:1, duration: 1}).to(".text-one",{opacity:0})
+    .to(".text-two",{opacity:1}).to(".text-two",{opacity:0})
+    .to(".text-three",{opacity:1}).to(".text-three",{opacity:0})
+    .to(".text-four",{opacity:1}).to(".text-four",{opacity:0})
+// const tl = gsap.timeline({
+//     scrollTrigger: {
+//       trigger: ".uno",
+//       start: "center center",
+//       pin: true,
+//       scrub: true,
+//       end: "bottom top",
+//       onEnter: () => M2.play(),
+//       onLeaveBack: () => M2.reverse()
+//     }
+//   }).to(".text-one", {opacity: 1}).to('.character2', {opacity: 1, duration: 2});
+  
+//   const tl2 = gsap.timeline({
+//     scrollTrigger: {
+//       trigger: "#dos",
+//       start: "center center",
+//       pin: true,
+//       scrub: true,
+//       end: "bottom top",
+//       onEnter: () => M4.play(),
+//       onEnterBack: () => M4.restart(),
+//       onLeave: () => M4.pause()
+//     }
+//   }).to(".text-two", {opacity: 1}).to(".text-two", {opacity: 0});
+//   const tl3 = gsap.timeline({
+//     scrollTrigger: {
+//       trigger: ".tres",
+//       start: "center center",
+//       pin: true,
+//       scrub: true,
+//       end: "bottom top",
+//       onEnter: () => M5.restart(),
+//       onEnterBack: () => M5.restart()
+//     }
+//   }).to(".text-three", {opacity: 1}).to('.character4', {opacity: 1, duration: 2}).to('.character4', {opacity: 0}).to(".text-three", {opacity: 0});
+//   const tl4 = gsap.timeline({
+//     scrollTrigger: {
+//       trigger: ".cuatro",
+//       start: "center center",
+//       pin: true,
+//       scrub: true,
+//       end: "bottom top",
+//       onEnter: () => M6.restart(),
+//       onEnterBack: () => M6.restart()
+//     }
+//   }).to(".text-four", {opacity: 1}).to('.character5', {opacity: 1, duration: 2}).to('.character5', {opacity: 0}).to(".text-four", {opacity: 0});
+
+
+
+
   // const tl3 = gsap.timeline({
   //   scrollTrigger: {
   //     trigger: "#dos",
